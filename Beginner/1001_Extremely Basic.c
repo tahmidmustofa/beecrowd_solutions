259d16b5e95fea7efe2a7dd3875e3192a09f3812
Tahmid Mustofa <tahmidmustofa7@gmail.com>
2024-10-24 22:47:16 +0600

    Create 1001_Extremely Basic.c

---
+++ Beginner/1001_Extremely Basic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int A;
+    int B;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    int X = A + B;
+    printf("X = %d\n", X);
+    
+    return 0;
+}
+
+// Solved by Tahmid Mustofa
