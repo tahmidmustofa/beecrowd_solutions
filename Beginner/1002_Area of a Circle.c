563ef02db53355256fbd0bc3e228415f80a1108c
Tahmid Mustofa <tahmidmustofa7@gmail.com>
2024-10-25 19:35:44 +0600

    Create  1002_Area of a Circle.c

---
+++ Beginner/1002_Area of a Circle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    double n = 3.14159;
+
+    double R;
+    scanf("%lf", &R);
+
+    double area = n * R * R;
+    printf("A=%.4lf\n", area);
+    
+    return 0;
+}
+
+// Solved By Tahmid Mustofa
