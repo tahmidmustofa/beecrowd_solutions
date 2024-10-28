1d27105e7788599c8fb3bd907d03283adc1b99c6
Tahmid Mustofa <tahmidmustofa7@gmail.com>
2024-10-28 08:25:40 +0600

    Create 1006_Average_2.c

---
+++ Beginner/1006_Average_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    float A, B, C;
+    scanf("%f %f %f", &A, &B, &C);
+    
+    float average = ((A * 2) + (B * 3) + (C * 5)) / (2 + 3 + 5);
+    printf("MEDIA = %.1f\n", average);
+    
+    return 0;
+    
+}
+
+// Solved by Tahmid Mustofa
