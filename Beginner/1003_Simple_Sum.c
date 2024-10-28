fa6a98d6a30aae5ad93f8b60006698fc6d3064fe
Tahmid Mustofa <tahmidmustofa7@gmail.com>
2024-10-28 07:30:16 +0600

    Create 1003_Simple_Sum.c

---
+++ Beginner/1003_Simple_Sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int A;
+    int B;
+    
+    scanf("%d %d", &A, &B);
+    
+    int SOMA = A + B;
+    
+    printf("SOMA = %d\n", SOMA);
+    
+    return 0;
+}
+
+// Solved By Tahmid Mustofa
