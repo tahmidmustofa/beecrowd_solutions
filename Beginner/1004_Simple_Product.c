e0e249fd7e0203ed80ae8a817ac6b341cae64f60
Tahmid Mustofa <tahmidmustofa7@gmail.com>
2024-10-28 07:39:41 +0600

    Create 1004_Simple_Product.c

---
+++ Beginner/1004_Simple_Product.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int A, B;
+    scanf("%d %d", &A, &B);
+    
+    int PROD = A * B;
+    printf("PROD = %d\n", PROD);
+    
+    return 0;
+    
+}
+
+// Solved by Tahmid Mustofa
