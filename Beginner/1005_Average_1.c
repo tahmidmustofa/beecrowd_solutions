f68f472aa7e115ac818fe1727a603701a810ada4
Tahmid Mustofa <tahmidmustofa7@gmail.com>
2024-10-28 08:09:51 +0600

    Create 1005_Average_1.c

---
+++ Beginner/1005_Average_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    double A, B;
+    scanf("%lf %lf", &A, &B);
+    
+    float average = ((A * 3.5) + (B * 7.5)) / (3.5 + 7.5);
+    printf("MEDIA = %.5lf\n", average);
+    
+    return 0;
+    
+}
+
+// Solved by Tahmid Mustofa
